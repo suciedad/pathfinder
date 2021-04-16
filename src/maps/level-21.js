2d4de91adef73957eb9fccebe9a1dd5954967e86
Dmitry Dolgov <pahuukorppi@gmail.com>
2021-04-17 00:25:37 +0300

    Add level 23

---
+++ src/maps/level-21.js
@@ -0,0 +1,35 @@
+const map = [
+  'xxxxxxxxx',
+  'xxRxxxxxx',
+  'xx--xxxxx',
+  'xx-F-xxxx',
+  'xG----xxx',
+  'xxx----xx',
+  'xxxx---Bx',
+  'xxxxxYxxx',
+  'xxxxxxxxx',
+];
+
+const players = [
+  {
+    color: 'red',
+    position: { x: 3, y: 5 },
+  },
+  {
+    color: 'blue',
+    position: { x: 2, y: 2 },
+  },
+  {
+    color: 'green',
+    position: { x: 4, y: 6 },
+  },
+  {
+    color: 'yellow',
+    position: { x: 2, y: 4 },
+  },
+];
+
+export default {
+  map,
+  players,
+};
--- src/stages/level-selection.js
+++ src/stages/level-selection.js
@@ -24,13 +24,14 @@ import level19 from '../maps/level-17';
 import level20 from '../maps/level-18';
 import level21 from '../maps/level-19';
 import level22 from '../maps/level-20';
+import level23 from '../maps/level-21';
 
 const levelsMap = {
   level1, level2, level3, level4, level5,
   level6, level7, level8, level9, level10,
   level11, level12, level13, level14, level15,
   level16, level17, level18, level19, level20,
-  level21, level22,
+  level21, level22, level23,
 };
 
 const TEXT_STYLE = {
